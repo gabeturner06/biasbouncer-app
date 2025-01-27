6a6dd946bb1bbb3c7d93297d53edcf8ec3f9ca9c
Gabe Turner <135384791+gabeturner06@users.noreply.github.com>
2025-01-27 09:42:03 -0500

    deleted tool

--- test_search_tool.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
-
-wrapper = DuckDuckGoSearchAPIWrapper(region="de-de", time="d", max_results=2)
-
-search = DuckDuckGoSearchResults(api_wrapper=wrapper, source="news")
-
-search.invoke("Obama")
